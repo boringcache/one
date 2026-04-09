ae9154b53c2b30a6d74c748e95dbea16c0ccb196
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-04-09 13:55:48 +0100

    Sync one dist utils bundle

--- dist/utils.js
+++ dist/utils.js
@@ -117,7 +117,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.12.21',
+        cliVersion: core.getInput('cli-version') || 'v1.12.22',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
