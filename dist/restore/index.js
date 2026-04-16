625df00badbbf645e0194062e0757fc6c335cfed
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-04-16 09:36:41 +0100

    chore: release 1.12.43

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -45624,7 +45624,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.12.29',
+        cliVersion: core.getInput('cli-version') || 'v1.12.31',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
