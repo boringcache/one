bf4f9dd2e443cdd0b445ac5f4ed6ca7b129752ce
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-04-16 17:08:04 +0100

    fix: align one CLI default with latest CLI release

--- dist/save/index.js
+++ dist/save/index.js
@@ -45709,7 +45709,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.12.31',
+        cliVersion: core.getInput('cli-version') || 'v1.12.32',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
