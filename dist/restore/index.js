f91b64833939f9eadceb3abfa47560badb275bba
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-04-20 14:50:56 +0100

    Release one v1.12.57

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -45797,7 +45797,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.12.39',
+        cliVersion: core.getInput('cli-version') || 'v1.12.40',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
