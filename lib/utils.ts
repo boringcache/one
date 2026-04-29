df3dac0287afac53011e441f2d5eb63b8b76745c
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-04-29 10:22:04 +0100

    Release action v1.12.77

--- lib/utils.ts
+++ lib/utils.ts
@@ -180,7 +180,7 @@ const TOOL_LABELS: Record<string, string> = {
 
 export function getInputs(): OneInputs {
   return {
-    cliVersion: core.getInput('cli-version') || 'v1.12.55',
+    cliVersion: core.getInput('cli-version') || 'v1.12.56',
     cliPlatform: core.getInput('cli-platform'),
     setup: normalizeSetup(core.getInput('setup')),
     mode: normalizeMode(core.getInput('mode')),
