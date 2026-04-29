a604be1b56328ace94a951e63f1a332755244d19
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-04-29 10:39:31 +0100

    Release action v1.12.78

--- dist/utils.js
+++ dist/utils.js
@@ -118,7 +118,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.12.56',
+        cliVersion: core.getInput('cli-version') || 'v1.12.57',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
