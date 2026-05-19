7a343f2961f28ca2b93852aaed1c3d080827febe
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-05-19 10:28:57 +0100

    chore: bump action to CLI v1.13.6

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -51697,7 +51697,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.13.5',
+        cliVersion: core.getInput('cli-version') || 'v1.13.6',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
