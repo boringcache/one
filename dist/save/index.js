4230b53b86aaed9a74e7994131347a2cfb082e6d
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-05-20 10:45:42 +0100

    Bump one default CLI to 1.13.11

--- dist/save/index.js
+++ dist/save/index.js
@@ -51812,7 +51812,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.13.10',
+        cliVersion: core.getInput('cli-version') || 'v1.13.11',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
