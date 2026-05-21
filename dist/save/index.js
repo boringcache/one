78f80d45710cdce00ce7045b102a36cefc345f33
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-05-21 14:16:20 +0100

    Update default CLI to v1.13.14

--- dist/save/index.js
+++ dist/save/index.js
@@ -51815,7 +51815,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.13.13',
+        cliVersion: core.getInput('cli-version') || 'v1.13.14',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
