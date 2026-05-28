fcea6fdfe75f42c9ce6e7663a29a15bae2db32bc
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-05-28 16:55:48 +0100

    Release one v1.13.16

--- lib/utils.ts
+++ lib/utils.ts
@@ -182,7 +182,7 @@ const TOOL_LABELS: Record<string, string> = {
 
 export function getInputs(): OneInputs {
   return {
-    cliVersion: core.getInput('cli-version') || 'v1.13.14',
+    cliVersion: core.getInput('cli-version') || 'v1.13.16',
     cliPlatform: core.getInput('cli-platform'),
     setup: normalizeSetup(core.getInput('setup')),
     mode: normalizeMode(core.getInput('mode')),
