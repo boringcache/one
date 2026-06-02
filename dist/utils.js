fe56ba81de1aa2fb877bc65035f76a5652206bbf
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-06-02 20:10:20 +0100

    Bump action CLI default to 1.13.27

--- dist/utils.js
+++ dist/utils.js
@@ -119,7 +119,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.13.26',
+        cliVersion: core.getInput('cli-version') || 'v1.13.27',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
