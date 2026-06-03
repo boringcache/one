3eda592eecf50b58f99d22cffe5cbc308014390c
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-06-03 04:01:03 +0100

    Bump action CLI default to 1.13.29

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -51977,7 +51977,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.13.28',
+        cliVersion: core.getInput('cli-version') || 'v1.13.29',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
