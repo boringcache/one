2870e3a90f3a75b665032b36b95ab1bcda76bd81
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-06-04 16:22:11 +0100

    Align one action with CLI 1.13.36

--- dist/utils.js
+++ dist/utils.js
@@ -119,7 +119,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.13.35',
+        cliVersion: core.getInput('cli-version') || 'v1.13.36',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
