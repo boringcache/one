8889852c69be30e08e1eb0790bacc5df3a60b348
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-06-04 20:13:47 +0100

    Align one action with CLI 1.13.37

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -51993,7 +51993,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.13.36',
+        cliVersion: core.getInput('cli-version') || 'v1.13.37',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
