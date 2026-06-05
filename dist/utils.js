4ba2a2c1afa5aa9a45b407674bf21456cfa884c6
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-06-05 10:47:38 +0100

    Default to CLI 1.13.40

--- dist/utils.js
+++ dist/utils.js
@@ -119,7 +119,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.13.39',
+        cliVersion: core.getInput('cli-version') || 'v1.13.40',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
