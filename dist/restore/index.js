52fcdf55af38c267ad8f5160ec21ea570d66254e
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-06-20 10:29:35 +0200

    Sync one action distribution for 1.13.58

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -51994,7 +51994,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.13.57',
+        cliVersion: core.getInput('cli-version') || 'v1.13.58',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
