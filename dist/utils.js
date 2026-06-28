24c1881123f8fa104ffd0e2ba6e42c04cc2cd78d
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-06-28 21:11:56 +0100

    Sync one action distribution for 1.13.72

--- dist/utils.js
+++ dist/utils.js
@@ -119,7 +119,7 @@ const TOOL_LABELS = {
 };
 function getInputs() {
     return {
-        cliVersion: core.getInput('cli-version') || 'v1.13.71',
+        cliVersion: core.getInput('cli-version') || 'v1.13.72',
         cliPlatform: core.getInput('cli-platform'),
         setup: normalizeSetup(core.getInput('setup')),
         mode: (0, modes_1.normalizeMode)(core.getInput('mode')),
